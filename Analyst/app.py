0cf74b5c92018d313f13a39e22dd13925e955d92
Djokovicxw <38768687@qq.com>
2019-01-18 17:08:23 +0800

    add errorhandler

--- Analyst/app.py
+++ Analyst/app.py
@@ -52,5 +52,10 @@ def gen_pie_img():
     return pie
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+    
+
 if __name__ == '__main__':
     manager.run()
